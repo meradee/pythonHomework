2734c740facd5b4aa5561148e5307019643f37d2
meradee <mailmintymint@gmail.com>
2020-03-29 13:00:34 -0700

    indention fixes

--- python-challenge/PyBank/main.py
+++ python-challenge/PyBank/main.py
@@ -6,10 +6,10 @@ csvpath = os.path.join('Resources', 'budget_data.csv')
 with open(csvpath, newline='') as csvfile:
 
   PyBank = csv.reader(csvfile, delimiter=',')
+
   csv_header = next(PyBank)
 
   with open("file.txt", "w") as outfile:
-
     dateList = []
     revenueList = []
 # Append the data into lists
@@ -18,33 +18,33 @@ with open(csvpath, newline='') as csvfile:
       revenueList.append(row[1])
 
 # Calcuate total months
-totalMonths = len(dateList)
-print("\nFinancial Analysis\n------------")
-print(f"Total Months: {totalMonths}")
-
+    totalMonths = len(dateList)
+    print("\nFinancial Analysis\n------------")
+    print(f"Total Months: {totalMonths}")
+    outfile.write(f"\nFinancial Analysis\n-------\nTotal Months: {totalMonths}")
 
 # Calculate total revenue
-sumRevenue = 0
-for revenue in revenueList:
-  sumRevenue += int(revenue)
+    sumRevenue = 0
 
-print("Total: $" + str(sumRevenue))
-outfile.write("\nTotal: $" + str(sumRevenue))
+    for revenue in revenueList:
+      sumRevenue += int(revenue)
+
+    print("Total: $" + str(sumRevenue))
+    outfile.write("\nTotal: $" + str(sumRevenue))
 
 # Calculate average change
-changeList = []
-changeSum = 0
+    changeList = []
+    changeSum = 0
 
 # For Loop Average Month change
 
-for i in range(totalMonths-1):
-	changeValue = int(revenueList[i+1]) - int(revenueList[i])
-	changeList.append(changeValue)
-	changeSum += changeValue
-
-	averageChange = changeSum/(totalMonths-1)
-
-	print("Average Change: $" + str(averageChange))
-  outfile.write("\nAverage Change $" + str(averageChange))
+    for i in range(totalMonths-1):
+	    changeValue = int(revenueList[i+1]) - int(revenueList[i])
+	    changeList.append(changeValue)
+	    changeSum += changeValue
+      
+    averageChange = changeSum/(totalMonths-1)
+    print("Average Change: $" + str(averageChange))
+    outfile.write("\nAverage Change $" + str(averageChange))
 
 # Calculate greatest increase and greatest decrease in revenue
