49013c1e782ce3c254837e3df89e0289db8af7ce
meradee <mailmintymint@gmail.com>
2020-04-01 15:33:54 -0700

    script updated

--- python-challenge/PyPoll/main.py
+++ python-challenge/PyPoll/main.py
@@ -5,12 +5,12 @@ csvpath = os.path.join('Resources', 'election_data.csv')
 
 with open(csvpath, newline='') as csvfile:
   pyPoll = csv.reader(csvfile, delimiter=',')
-
   csvHeader = next(pyPoll)
 
   votesList =[]
   candidateList =[]
 
+#List name for candidates
   for row in pyPoll:
     votesList.append(row[2])
     if row[2] not in candidateList:
@@ -24,3 +24,20 @@ with open(csvpath, newline='') as csvfile:
 
     outfile.write("Election Results")
     outfile.write("Total Votes: " + str(totalVotes))
+
+#Candidate name count 
+    votesTally = {candidate:votesList.count(candidate) for candidate in candidateList}
+#Candidate percent votes and number of votes
+    percentVotes = {candidate:((votesCount/totalVotes)*100) for candidate, votesCount in votesTally.items()}
+
+#Print name of candidate, percent of votes, number of votes with for loop
+    for candidate, x in percentVotes.items():
+        for k,v in votesTally.items():
+          if k==candidate:
+            print("".join("{}: {:5.2f}% ({})".format(k,x,v)))
+            outfile.write("".join("{}: {:5.2f}% ({})\n".format(k,x,v)))
+
+#Print votes in format
+    print("".join("Winner:{}".format(candidate) for candidate, votesCount in votesTally.items() if votesList == max (votesTally.values())))
+
+    outfile.write("".join("Winner:{}".format(candidate) for candidate, votesCount in votesTally.items() if votesCount == max(votesTally.values())))
